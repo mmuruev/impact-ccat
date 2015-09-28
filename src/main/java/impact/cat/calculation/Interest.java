cab94e1ab1bc8baf071ef9bfe162f56154a6c32c
maksim <maksim.muruev@creditstargroup.com>
2015-09-28 13:48:50 +0300

    = fix sum interest percentage

--- src/main/java/impact/cat/calculation/Interest.java
+++ src/main/java/impact/cat/calculation/Interest.java
@@ -56,7 +56,7 @@ public class Interest {
     public Money getFinalInterest(){
         Money finalInterest = new Money();
         for(int day= FIRST_INTEREST_DAY; day< days ; day++){
-           finalInterest.add(getAmountForDay(day));
+           finalInterest = finalInterest.add(getAmountForDay(day));
         }
         return finalInterest;
     }
